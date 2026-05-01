89ffabb26f79809394fadf676aa0593659e806d4
Skye J <skye@skyenet.online>
2026-05-01 09:39:18 -0400

    fix(claude)!: rename to claude

--- pkgs/by-name/cl/claude/package.nix
+++ pkgs/by-name/cl/claude/package.nix
@@ -12,8 +12,8 @@ stdenvNoCC.mkDerivation (
     version2 = "9a9e3d5a4a368f0f49a80dc303b0ed1a18bfedad";
   in
   {
-    version = "${version1}-${version2}";
-    pname = "claude-desktop";
+    version = "1.5354.0,9a9e3d5a4a368f0f49a80dc303b0ed1a18bfedad";
+    pname = "claude";
     src = fetchzip {
       url = "https://downloads.claude.ai/releases/darwin/universal/${version1}/Claude-${version2}.zip";
       hash = "sha256-mEObbChAVBFhBnrjn+6tt0cjI3YiKmAqlDO9K/rOprg=";
