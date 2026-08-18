44f47ed71e2371a6ce7967910f712b97c155b2a1
Skye J <skye@skyenet.online>
2026-08-18 16:59:04 -0400

    fix: patch -x

--- pkgs/by-name/nh/nhfourk/package.nix
+++ pkgs/by-name/nh/nhfourk/package.nix
@@ -5,12 +5,14 @@
   bison,
   zlib,
   flex,
+  jansson,
 }:
 let
   pkgs = (builtins.getFlake "github:nixos/nixpkgs/nixos-22.11").legacyPackages."${stdenv.system}";
 in
-pkgs.stdenv.mkDerivation {
+stdenv.mkDerivation {
   name = "nhfourk";
+  hardeningDisable = [ "format" ];
   src = fetchFromGitHub {
     owner = "tsadok";
     repo = "nhfourk";
@@ -18,12 +20,16 @@ pkgs.stdenv.mkDerivation {
     hash = "sha256-tfCOFPje9VI4dkJyHTm4InYNePAUsFUfeTszXrUn3BA=";
   };
 
+  patches = [
+    ./patch1.patch
+  ];
   nativeBuildInputs = [
-    pkgs.SDL2
-    pkgs.zlib
-   pkgs.flex
-   pkgs.bison
+    SDL2
+    zlib
+    flex
+    bison
     pkgs.perl
+    jansson
   ];
   buildPhase = ''
     runHook preBuild
@@ -31,8 +37,17 @@ pkgs.stdenv.mkDerivation {
     patchShebangs --build aimake scripts/*
 
     cd build
-    ../aimake
 
+    ../aimake --without=jansson --without=gui -B fhs
     runHook postBuild
+  '';
+  installPhase = ''
+  runHook preInstall
+  cd build
+  mkdir -p $out
+
+  ../aimake --without=jansson --without=gui --install-only -B fhs -i $out
+
+  runHook postInstall
   '';
 }
